c4243dfd3904e0155855106bf12f4193a6a843a9
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-05-26 05:27:31 -0400

    add googletest

--- tests/wxWidgets/window/main.cpp
+++ tests/wxWidgets/window/main.cpp
@@ -1,43 +1,29 @@
+/**
+ * wxWidgets Window Unit Test.
+ * Expected: Start a wxApp that creates an empty wxFrame then closes it.
+ */
+
 #include <wx/wx.h>
 
-class MyFrame : public wxFrame
+class TestFrame : public wxFrame
 {
 public:
-    MyFrame(const wxString &title)
+    TestFrame(const wxString &title)
         : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(300, 200))
     {
-        // Create a menu bar
-        wxMenuBar *menuBar = new wxMenuBar;
-        wxMenu *fileMenu = new wxMenu;
-        fileMenu->Append(wxID_EXIT, "&Exit\tCtrl+Q", "Quit the application");
-        menuBar->Append(fileMenu, "&File");
-        SetMenuBar(menuBar);
-
-        // Create a status bar
-        CreateStatusBar();
-        SetStatusText("Welcome to wxWidgets!");
-
-        // Connect the "Exit" menu item to the OnExit event handler
-        Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-    }
-
-private:
-    void OnExit(wxCommandEvent &event)
-    {
-        Close(true);
     }
 };
 
-class MyApp : public wxApp
+class TestApp : public wxApp
 {
 public:
     virtual bool OnInit() override
     {
-        MyFrame *frame = new MyFrame("Simple wxWidgets Window");
-        frame->Show(true);
+        TestFrame *frame = new TestFrame("Test Window");
+        frame->Close(true);
         return true;
     }
 };
 
 #define WinMain main
-wxIMPLEMENT_APP(MyApp);
+wxIMPLEMENT_APP(TestApp);
